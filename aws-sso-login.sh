68131c3f6818c2ed92ae86c1c0a1910172ea227a
Khalifah Shabazz <619281+b01@users.noreply.github.com>
2025-05-12 07:17:24 -0400

    add: AWS SSO Login Script For Linux/Mac
    This commit adds a script to facilitate AWS SSO login for Linux and Mac
    users. The script automates the process of logging into AWS from the CLI
    using SSO credentials set in the environment. Making it easier for users
    to access their AWS resources without having to manually enter their
    credentials each time.

---
+++ aws-sso-login.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+logged_in=""
+
+# Function to check AWS SSO login status
+check_aws_sso_login() {
+    profile_name="${1}"
+
+    # Attempt to retrieve the AWS account ID using sts get-caller-identity
+    output="$(aws sts get-caller-identity --profile "${profile_name}" 2>&1)"
+
+    if [ "${?}" -ne 0 ]; then
+        case "${output}" in
+            *ExpiredToken*)
+                echo "AWS SSO token has expired. Please renew your login using 'aws sso login'."
+                ;;
+            *)
+                echo "AWS credentials not found or an error occurred. Please login using 'aws sso login'."
+                ;;
+        esac
+        logged_in="no"
+    else
+        arn=$(echo "${output}" | awk -F'"' '/Arn/ {print $4}')
+        echo "Successfully logged in as ${arn}"
+        logged_in="yes"
+    fi
+}
+
+profile="${AWS_PROFILE}"
+
+if [ -n "${1}" ]; then
+  profile="${1}"
+fi
+
+if [ -z "${profile}" ]; then
+    echo "missing AWS profile, pass in as the first argument or set AWS_PROFILES"
+fi
+
+check_aws_sso_login "${profile}"
+
+if [ "${logged_in}" = "no" ]; then
+    aws sso login --use-device-code
+    check_aws_sso_login "${profile}"
+fi
